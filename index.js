2095b6b831f2f8abe05a0ce71a974cb74c1673a8
vomvoru <qkrcjfgus3@gmail.com>
2019-08-30 18:29:49 +0900

    함수 최대 길이 규칙 수정

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ module.exports = {
     // 80자 제한 이유: https://www.codereadability.com/maximum-line-length/
     'max-lines': ['warn', { max: 80, skipBlankLines: true, skipComments: true }],
 
-    'max-lines-per-function': ['error', { max: 20, skipBlankLines: true, skipComments: true, IIFEs: true }],
+    'max-lines-per-function': ['error', { max: 10, skipBlankLines: true, skipComments: true, IIFEs: true }],
     'max-depth': ['warn', 2],
     'import/no-extraneous-dependencies': [
       'error',
